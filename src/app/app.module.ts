be51991744f2e2cc3a79964c8e3605bb4513f1bb
Aga <aga@MacBook-Pro-Aga.local>
2023-03-07 20:02:25 +0100

    lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,9 @@
-import { AuthModule } from './auth/auth.module';
-import { DataStorageService } from './shared/data-storage.service';
-import { RecipeService } from './recipes/recipe.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { RecipesRoutingModule } from './recipes/recipes-routing.module';
