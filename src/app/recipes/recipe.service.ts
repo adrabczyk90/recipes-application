734806644e300a28c5f9dc120cb3bf417eb17950
Aga <aga@MacBook-Pro-Aga.local>
2023-03-08 12:44:02 +0100

    changing code to use ngrx

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,11 @@
+import { AppState } from './../shopping-list/store/shopping-list.reducer';
+import { Store } from '@ngrx/store';
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
+import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer'
 
 @Injectable()
 export class RecipeService{
@@ -29,7 +32,7 @@ export class RecipeService{
 
     private recipes: Recipe[] = [];
 
-    constructor(private slService: ShoppingListService){
+    constructor(private store: Store<fromShoppingList.AppState>){
     }
 
     setRecipes(recipes: Recipe[]){
@@ -47,7 +50,7 @@ export class RecipeService{
     }
 
     addIngredientsToShoppingList(ingredients : Ingredient []) {
-    this.slService.addIngredients(ingredients);
+      this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
     }
 
     addRecipe(recipe: Recipe){
